673c462064945cfd071bffb02ad4aa21a1ba723b
axzoo <jun.cyc2018@gmail.com>
2018-10-20 21:33:58 +0800

    ver0.1

---
+++ app/src/main/java/nasa2018hkth/app_02/Main2Activity.java
@@ -0,0 +1,23 @@
+package nasa2018hkth.app_02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+            }
+        } {
+
+        }
+        }
+        }
